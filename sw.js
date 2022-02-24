8a4d128c19035cd1894b156ec613955aa9e79ca0
4hosting <hamdaniftikhar.96@gmail.com>
2022-02-25 00:44:48 +0530

    GoogleTag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220223.2326';
+const cacheName = 'chirpy-20220225.0041';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
