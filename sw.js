3ad951ae9c74e924750ad184295c44ed0cf35d1f
4hosting <94830212+4hosting@users.noreply.github.com>
2022-09-07 02:43:19 +0530

    bending spoons

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220315.1301';
+const cacheName = 'chirpy-20220907.0234';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
