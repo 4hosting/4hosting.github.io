ecd12a062237f4aa7bff38f975612f4397afde57
4hosting <94830212+4hosting@users.noreply.github.com>
2022-09-07 10:55:52 +0530

    pointerfix and slide_deck_bs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220907.0234';
+const cacheName = 'chirpy-20220907.1049';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
